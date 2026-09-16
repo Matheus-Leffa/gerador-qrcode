cbcca8b200ce72ca13f070ffcf1cff8df60e441b
Matheus-Leffa <matheusleffa@live.com>
2026-09-16 16:27:38 -0300

    controller e dockerfile

--- src/main/java/com/matheusleffa/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/matheusleffa/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.matheusleffa.qrcode.generator.controller;
 
 import com.matheusleffa.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.matheusleffa.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.matheusleffa.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
+
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
